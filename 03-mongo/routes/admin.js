c5760536bc24b4514d26d9b8f1167f9893bf4973
TusharKhichawat <155963624+TusharKhichawat@users.noreply.github.com>
2025-03-10 19:22:52 +0530

    Solutions

--- 03-mongo/routes/admin.js
+++ 03-mongo/routes/admin.js
@@ -1,18 +1,57 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require('../db/index');
 const router = Router();
+
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //check if the user with this username is already exits
+    Admin.create({
+        username:username,
+        password:password
+    })
+    .then(function(){
+        res.json({message:"Admin created successfully"});
+    })
+    .catch(function(){
+        res.json({message:"Failed to create admin"});
+    })
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title=req.body.title;
+    const description=req.body.description;
+    const price=req.body.price;
+    const imageLink=req.body.price;
+
+    const newCourse= await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+    res.json({
+        message: "Course created successfully",
+        courseId:newCourse._id
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({})
+        .then(function(response){
+            res.json({
+                courses:response
+            });
+        })
+    
 });
 
 module.exports = router;
--- 04-mongo-with-jwt-auth/routes/admin.js
+++ 04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,49 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin} =require("../db")
 const router = Router();
+const {JWT_SECRET}=require("..");
+const jwt= require("jsonwebtoken")
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //check if the user with this username is already exits
+    Admin.create({
+        username:username,
+        password:password
+    })
+    .then(function(){
+        res.json({message:"Admin created successfully"});
+    })
+    .catch(function(){
+        res.json({message:"Failed to create admin"});
+    })
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const user=await User.find({
+        username,
+        password
+    })
+    if(user){
+        const token= jwt.sign({
+            username
+        }, JWT_SECRET);
+        res.json({token})
+    }else{
+        res.status(411).json({
+            message:"Incorrect email and pass"
+        })
+    }
+    
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
@@ -18,5 +53,5 @@ router.post('/courses', adminMiddleware, (req, res) => {
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
 });
-
+ 
 module.exports = router;
